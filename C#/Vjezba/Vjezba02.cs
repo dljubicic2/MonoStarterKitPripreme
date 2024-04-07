c5fd16be73736c0d5b6f5b40a1f43f13ab9f899e
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2024-04-07 21:36:44 +0200

    For petlja

---
+++ C#/Vjezba/Vjezba02.cs
@@ -0,0 +1,29 @@
+﻿namespace Vjezba02IterationStatement
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+         
+            for(int i = 0; i<10; i++)
+            {
+                //Console.WriteLine(i);
+                if(i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+            }
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+            
+            
+            Console.ReadLine();
+
+           
+        }
+       
+    }
+}
