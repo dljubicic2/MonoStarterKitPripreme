4d4645a7ae3deb193a7684569ab43fadceaca697
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2024-04-15 21:38:20 +0200

    Guessing game

---
+++ C#/Projekti/IgraPogadjanjaBrojeva.cs
@@ -0,0 +1,48 @@
+﻿namespace Project02GuessingGame
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool isCorrectGuess = false;
+
+            Random random = new Random();
+            int randomNumber = new Random().Next(1,11);
+            
+            Console.WriteLine("**************************");
+            Console.WriteLine("***Guessing number game***");
+            Console.WriteLine("**************************");
+
+            Console.WriteLine("A number between 1 and 10 will be generated: ");
+            Console.WriteLine("If you guess the correct number, you will win: ");
+
+            Console.WriteLine(randomNumber);
+
+            while (!isCorrectGuess)
+            {
+                Console.Write("Enter your guess: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if(guess > randomNumber)
+                {
+                    Console.WriteLine("Your guess is wrong! ");
+                }
+                else if(guess < randomNumber)
+                {
+                    Console.WriteLine("Your guess is wrong! ");
+                }
+                else
+                {
+                    Console.WriteLine("Correct! ");
+
+                    isCorrectGuess = true;
+                }
+            }
+
+            Console.WriteLine("Congratulations you won the game! ");
+
+
+            Console.ReadKey();
+        }
+    }
+}
