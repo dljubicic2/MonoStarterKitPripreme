eeaed66c9fcea9be8f89a5a37506a5a028df874c
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2024-04-16 15:07:45 +0200

    Jednostavan primjer metode i pozivanje metode

---
+++ C#/Vjezba/Vjezba05.cs
@@ -0,0 +1,18 @@
+﻿namespace Vjezba05Metode
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+
+            
+        }
+    }
+}
