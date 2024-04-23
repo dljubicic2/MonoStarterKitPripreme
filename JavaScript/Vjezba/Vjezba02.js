e673e239b1b3b9ba8920f291a3f5a9494dd556a7
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2024-04-23 11:16:15 +0200

    Aritmetički operatori

---
+++ JavaScript/Vjezba/Vjezba02.js
@@ -0,0 +1,38 @@
+// Aritmetički operatori
+
+let students = 20;
+
+/*students = students +1;
+students = students -1;
+students = students * 2;
+students = students / 2;
+students = students ** 2;
+let extraStudents = students % 3; */
+
+// Jednostavniji način
+
+students += 3;
+students -=3;
+students *= 2;
+students /= 2;
+students **= 2;
+students %= 2;
+
+// Inkrement i dekrement
+
+students++;
+students--;
+
+// Redoslije aritmetičkih operatora
+    /*
+    1. Zagrade ()
+    2. Eksponenti
+    3. Množenje, djeljenje i modulo
+    4. Zbrajanje i oduzimanje
+    */
+
+let resault = 1 + 2 * 3 + 4 ** 2;
+console.log(resault);
+
+let resault2 = 12 % 5 + 8 / 2;
+console.log(resault2);
