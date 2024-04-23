f495b2ef059e295b1c4b8ea0d920d9431357d748
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2024-04-23 11:02:43 +0200

    Varijable

---
+++ JavaScript/Vjezba/Vjezba01.js
@@ -0,0 +1,29 @@
+// Varijable
+
+let x; // Let je funkcija za deklariranje varijable
+x = 100;
+console.log(x);
+
+// Int i decimal
+//let age = 22;
+let price = 9.99;
+let gpa =2.1;
+
+// String
+let firstName = "John";
+let favoriteFood = "Hamburger";
+let email = "john@gmail.com";
+
+// Bool
+let online = true;
+let forSale = false;
+//let isStudent = true;
+
+// Vježbe s varijablama
+let fullName = "John John";
+let age = 22;
+let isStudent = false;
+
+console.log("Your name is: ${fullName}");
+console.log("You are ${age} years old");
+console.log("John is ${isStudent}");
