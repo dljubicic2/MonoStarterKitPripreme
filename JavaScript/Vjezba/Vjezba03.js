d754d80cd54a5b05c42f87684dee4e2b8a75baa8
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2024-04-29 22:31:47 +0200

    Math funkcija

---
+++ JavaScript/Vjezba/Vjezba03.js
@@ -0,0 +1,42 @@
+// Math 
+
+// 1. primjer
+/*
+Math.PI;
+console.log(Math.PI);
+*/
+
+/*
+Math.E;
+console.log(Math.E);
+*/
+
+// 2. primjer
+
+let x = 3;
+let y = 2;
+let z =1;
+
+//z = Math.round(x); // round zaokružuje decimalni broj u cijeli broj
+//z = Math.floor(x); // floor zaokružuje decimalni broj u cijeli broj ali na manje
+//z = Math.ceil(x);  // ceil zaokružuje decimlani broj u cijeli broj ali na više
+//z = Math.trunc(x);  // trunc eliminira decimalna mjesta
+//z = Math.pow(x,y);
+//z = Math.sqrt(x); // sqrt izračunava korijen
+//z = Math.log(x);  // log je logaritam
+//z = Math.abs(x);    // abs izračunava apsolutne brojeve
+//z = Math.sign(x);   // sign prikazuje da li je broj pozitivan ili negativan
+
+/*
+z = Math.sin(x);
+z = Math.cos(x);    // Računanje trigonometrije
+z = Math.tan(x);
+*/
+
+/*
+let max = Math.max(x,y,z);
+console.log(max);
+                            // Prikazuje maksimalni ili minimalni broj
+let min = Math.min(x,y,z);
+console.log(min);
+*/
